79eed1795d535d59581f7544e758915b908aa46c
igorkll <69525332+igorkll@users.noreply.github.com>
2022-06-07 16:23:54 +0300

    d

--- libs/gui.lua
+++ libs/gui.lua
@@ -1,3 +1,5 @@
+local colors = require("colors")
+
 local function table_remove(tbl, obj)
     for i = 1, #tbl do
         if tbl[i] == obj then
@@ -12,6 +14,21 @@ local lib = {}
 lib.scenes = {}
 lib.scene = false
 
+lib.posXadd = 0
+lib.posYadd = 1
+
+function lib.drawUi()
+    local rx, ry = gpu.getResolution()
+    gpu.setBackground(colors.purple)
+    gpu.fill(1, 1, rx, ry, " ")
+end
+
+function lib.maxResolution()
+    local mx, my = gpu.maxResolution()
+    my = my - 2
+    return mx, my
+end
+
 function lib.createScene(color, resx, resy)
     local scene = {}
     scene.objs = {}
@@ -20,15 +37,21 @@ function lib.createScene(color, resx, resy)
     scene.removeCallbacks = {}
     scene.removed = false
 
+    scene.color = color
+    scene.resx = resx
+    scene.resy = resy
+
     function scene.checkRemove()
         if scene.removed then error("this scene removed", 0) end
     end
 
     function scene.draw()
         scene.checkRemove()
-        for i, v in ipairs(scene.objs) do
-            v.draw()
-        end
+        gpu.setResolution(scene.resy, scene.resy + 2)
+        lib.drawUi()
+        gpu.setBackground(scene.color)
+        gpu.fill(1 + lib.posXadd, 1 + lib.posYadd, scene.resx, scene.resy, " ")
+        for i, v in ipairs(scene.objs) do v.draw() end
     end
 
     function lib.select()
@@ -42,8 +65,22 @@ function lib.createScene(color, resx, resy)
         scene.checkRemove()
         for i, v in ipairs(scene.leaveCallbacks) do v() end
         for i, v in ipairs(scene.removeCallbacks) do v() end
+        table_remove(lib.scenes, lib)
         scene.removed = true
     end
+
+    function lib.uploadEvent(...)
+        scene.checkRemove()
+        for i, v in ipairs(scene.objs) do v.uploadEvent(...) end
+    end
+
+    --------------------------------------------
+
+    function lib.createButton(x, y, sizeX, sizeY, text, callback)
+        
+    end
+
+    --------------------------------------------
 
     return scene
 end
