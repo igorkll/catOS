bd388a0a8c06fcf4a5fa7da00eb1590a9c5f0c14
igorkll <69525332+igorkll@users.noreply.github.com>
2022-06-07 13:27:14 +0300

    a

--- init.lua
+++ init.lua
@@ -1,14 +1,44 @@
+--------------------------------------------main
+
 function getCp(ctype)
     return component.proxy(component.list(ctype)() or "")
 end
 
-_G.gpu = component.proxy(component.list("gpu")())
+function createEnv()
+    local env = {}
+    setmetatable(env, {__index = function(_, key)
+        return _G[key]
+    end})
+    env.load = function(data, name, mode, lenv)
+        if not lenv then lenv = env end --для коректной таблицы env
+        return load(data, name, mode, lenv)
+    end
+    return env
+end
+
+--------------------------------------------raw
+
+local function raw_loadfile(path, mode, env)
+    
+end
+
+--------------------------------------------graphic
+
+gpu = getCp("gpu")
 gpu.bind(component.list("screen")(), true)
 
-_G.palette = {0x000000, 0x444444, 0x888888, 0xBBBBBB, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF6600, 0x00FFFF, 0xFF00FF, 0x000000, 0x000000, 0x000000, 0x000000}
-local function setpalette(lpalette)
+palette = {0x000000, 0x444444, 0x888888, 0xBBBBBB, 0xFFFFFF, 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0xFF6600, 0x00FFFF, 0xFF00FF, 0x000000, 0x000000, 0x000000, 0x000000}
+function setpalette(lpalette)
     for i, v in ipairs(lpalette or palette) do
-        _G.gpu.setPaletteColor(i - 1, v)
+        gpu.setPaletteColor(i - 1, v)
     end
 end
 setpalette()
+
+--------------------------------------------libs
+
+loadedLibs = {}
+function require(name)
+    loadedLibs[name] = 
+    return loadedLibs[name]
+end
